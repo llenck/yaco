f720e08b7f7599a529fcc7bee46f0f942f2fc43b
Lars Lenckowski <larslenckowski@gmail.com>
2020-07-15 09:52:52 +0000

    make asm more readable

--- lib/yaco-asm.s
+++ lib/yaco-asm.s
@@ -12,8 +12,10 @@ yaco_create:
 	@ save previous environment to the coroutine state given as our first argument
 	stm r0, {r4-r11, sp, lr}
 
-	@ load new stack pointer (base + len, since the stack grows down). don't store this
-	@ in sp yet since sp might have a requirement to be 16-aligned
+	@ load new stack pointer (base + len, since the stack grows down). Don't store this
+	@ in sp yet since sp might have a requirement to be 16-aligned. This will cause sp to
+	@ point to the next element that doesn't belong to the stack, which is exactly right
+	@ since on armv7, we use use a full-descending stack
 	add r12, r3, r12
 	@ round the stack pointer down to a multiple of 16
 	lsl r12, r12, #4
@@ -44,13 +46,16 @@ yaco_switch:
 
 	@ here, we need to use r2-r9 instead of r4-r11 so we still have a different
 	@ register than r12 free that is above the 8 registers we store most of the
-	@ registers in. this is necesaary because r12 is already in use and the registers
+	@ registers in. This is necessary because r12 is already in use and the registers
 	@ used with stm/ldm must be in ascending order
 
 	@ load saved registers (use r10 instead of sp, since we still need sp)
 	ldm r0, {r2-r9, r10, lr}
-	@ and save them to the stack, below the registers we want to swap with
-	stmfd sp, {r2-r9, r10, lr}
+	@ and save them to the stack, below the registers we want to swap with.
+	@ this store and the corresponding load instruction will cause valgrind to scream at
+	@ you; I believe this is because valgrind interprets stores/loads below sp as an
+	@ array underflow. This can thus be safely ignored
+	stmdb sp, {r2-r9, r10, lr}
 
 	@ now load the registers we need to save again
 	ldm sp, {r4-r11, lr}
@@ -58,9 +63,7 @@ yaco_switch:
 	stm r0, {r4-r11, r12, lr}
 
 	@ finally, load the registers we actually want to have from the stack
-	@ (we of course have a full descending stack, but since sp currently points to
-	@ the next byte above what we want to load, ldmea is the right choice here)
-	ldmea sp, {r4-r11, r12, lr}
+	ldmdb sp, {r4-r11, r12, lr}
 	mov sp, r12
 
 	@ "return" to the function/coroutine/whatever
